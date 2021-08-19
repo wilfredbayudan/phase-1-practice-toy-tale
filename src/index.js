117c3b166f7fbb2c38336c2b1ad2b896b7386d4b
Wilfred <wilfredbayudan@gmail.com>
2021-08-18 14:51:10 -1000

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 let addToy = false;
+const toysUrl = 'http://localhost:3000/toys';
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -12,4 +13,98 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  const toyContainer = document.querySelector('div#toy-collection');
+  // fetch toys
+  function renderToy(id, name, image, likes) {
+    const card = document.createElement('div');
+    card.className = 'card';
+    const h2 = document.createElement('h2');
+    h2.textContent = name;
+    card.appendChild(h2);
+    const img = document.createElement('img');
+    img.src = image;
+    img.className = 'toy-avatar';
+    card.appendChild(img);
+    const p = document.createElement('p');
+    p.textContent = parseInt(likes) + ' likes';
+    card.appendChild(p);
+    const button = document.createElement('button');
+    button.setAttribute('id', id);
+    button.className = 'like-btn';
+    button.textContent = 'like';
+    button.addEventListener('click', likeToy);
+    card.appendChild(button);
+    toyContainer.append(card);
+  }
+  fetch(toysUrl)
+    .then(res => res.json())
+    .then(toys => {
+      toys.forEach(toy => renderToy(toy.id, toy.name, toy.image, toy.likes))
+    })
+    .catch(err => console.log(err));
+
+  // like a toy
+  function likeToy() {
+    const toyId = this.id;
+    fetch(toysUrl + `/${toyId}`)
+      .then(res => res.json())
+      .then(data => {
+        let currentLikes = data.likes;
+        updateToy(toyId, currentLikes);
+      })
+      .catch(err => console.log(err));
+  }
+
+  function updateToy(toyId, currentLikes) {
+    const likeToyConfig = {
+      method: 'PATCH',
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        likes: currentLikes + 1
+      })
+    }
+    fetch(toysUrl + `/${toyId}`, likeToyConfig)
+      .then(res => res.json())
+      .then(updatedToy => {
+        // UPDATE TOY ON DOM
+        const parentCard = document.getElementById(toyId).parentElement;
+        const p = parentCard.querySelector('p');
+        p.textContent = `${updatedToy.likes} likes`;
+      })
+      .catch(err => console.log(err));
+  }
+
+  // add toy
+  const form = document.querySelector('form.add-toy-form');
+
+  form.addEventListener('submit', e => {
+    const inputName = document.querySelector('input[name="name"]').value;
+    const inputImg = document.querySelector('input[name="image"]').value;
+    e.preventDefault();
+
+    const newToyConfig = {
+      method: 'POST',
+      headers: {
+        "Content-Type": 'application/json',
+        Accept : 'application/json'
+      },
+      body: JSON.stringify({
+        "name": inputName,
+        "image": inputImg,
+        "likes": 0
+      })
+    };
+    fetch(toysUrl, newToyConfig)
+      .then(res => res.json())
+      .then(newToy => renderToy(newToy.id, newToy.name, newToy.image, newToy.likes))
+      .catch(err => console.log(err));
+
+
+    form.reset();
+  })
+
 });
